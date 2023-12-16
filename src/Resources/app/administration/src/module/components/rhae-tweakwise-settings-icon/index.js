c0cd4fe0dc2a86122a62a5ce066d57621a53412d
Richard Haeser <richard@richardhaeser.com>
2023-12-16 20:42:06 +0100

    [TASK] Update JS

--- src/Resources/app/administration/src/module/components/rhae-tweakwise-settings-icon/index.js
+++ src/Resources/app/administration/src/module/components/rhae-tweakwise-settings-icon/index.js
@@ -1,8 +1,6 @@
 import template from './rhae-tweakwise-settings-icon.html.twig';
 import './rhae-tweakwise-settings-icon.scss'
 
-const { Component } = Shopware;
-
-Component.register('rhae-tweakwise-settings-icon', {
+Shopware.Component.register('rhae-tweakwise-settings-icon', {
     template,
 });
