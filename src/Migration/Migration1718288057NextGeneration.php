60482c9d81a314c3e9a5b627ad0b29c9dda30c91
Richard Haeser <richard@richardhaeser.com>
2024-06-20 12:04:43 +0200

    Shopware66 (#42)
    * [TASK] Shopware 6.6 compatibility
    
    * [TASK] CI
    
    * [TASK] Linting in CI
    
    * [TASK] CGL fixes
    
    * [TASK] Bump dependencies
    
    * [TASK] Release preparations
    
    * [FEATURE] Feed generation with scheduled task
    
    * [BUGFIX] Fix issue with feed generation
    
    * [BUGFIX] Fixed linting
    
    * [BUGFIX] Fixed linting
    
    * [BUGFIX] Don't add categories that are set to product_stream type
    
    (cherry picked from commit 78ba1296617f19467b3a7b1d504de373871cb45b)

---
+++ src/Migration/Migration1718288057NextGeneration.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace RH\Tweakwise\Migration;
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Framework\Log\Package;
+use Shopware\Core\Framework\Migration\MigrationStep;
+
+/**
+ * @internal
+ */
+#[Package('core')]
+class Migration1718288057NextGeneration extends MigrationStep
+{
+    public function getCreationTimestamp(): int
+    {
+        return 1718288057;
+    }
+
+    public function update(Connection $connection): void
+    {
+        $sql = <<<SQL
+ALTER TABLE `s_plugin_rhae_tweakwise_feed`
+                ADD `next_generation_at` DATETIME(3),
+                ADD `status` VARCHAR(255) NULL,
+                ADD `interval` VARCHAR(10) NULL
+                ;
+SQL;
+        $connection->executeUpdate($sql);
+    }
+}
