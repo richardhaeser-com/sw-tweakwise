121db137832b7f261da8cc180cf8680bb06ed5ce
Richard Haeser <richard@richardhaeser.com>
2025-02-09 13:01:11 +0100

    [BUGFIX] Fix issue with cross-selling

--- src/Service/ProductDataService.php
+++ src/Service/ProductDataService.php
@@ -40,9 +40,15 @@ class ProductDataService
     public function getProductShownInListing(ProductEntity $product, SalesChannelContext $context): ProductEntity
     {
         $criteria = new Criteria();
-        $criteria->addFilter(
-            new EqualsFilter('parentId', $product->getParentId())
-        );
+        if (!$product->getParentId()) {
+            $criteria->addFilter(
+                new EqualsFilter('id', $product->getId())
+            );
+        } else {
+            $criteria->addFilter(
+                new EqualsFilter('parentId', $product->getParentId())
+            );
+        }
         $criteria->addAssociation('options');
         $criteria->addAssociation('options.group');
         $criteria->setOffset(0);
