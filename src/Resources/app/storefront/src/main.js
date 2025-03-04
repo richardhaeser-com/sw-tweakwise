4e0172119ad4a64b6b5f1c8c42a79b26f1bac094
Richard Haeser <richard@richardhaeser.com>
2025-03-04 13:45:22 +0100

    WIP

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -2,5 +2,18 @@ import TwAddToCartPlugin from "./tw-add-to-cart/tw-add-to-cart-plugin";
 import TwAddToFavoritesPlugin from "./tw-add-to-favorites/tw-add-to-favorites-plugin";
 
 const PluginManager = window.PluginManager;
-PluginManager.register('TwAddToCartPlugin', TwAddToCartPlugin)
-PluginManager.register('TwAddToFavoritesPlugin', TwAddToFavoritesPlugin)
+try {
+    if (!PluginManager.getPlugin('TwAddToCartPlugin')) {
+        PluginManager.register('TwAddToCartPlugin', TwAddToCartPlugin)
+    }
+} catch (error) {
+    PluginManager.register('TwAddToCartPlugin', TwAddToCartPlugin)
+}
+
+try {
+    if (!PluginManager.getPlugin('TwAddToFavoritesPlugin')) {
+        PluginManager.register('TwAddToFavoritesPlugin', TwAddToFavoritesPlugin)
+    }
+} catch (error) {
+    PluginManager.register('TwAddToFavoritesPlugin', TwAddToFavoritesPlugin)
+}
