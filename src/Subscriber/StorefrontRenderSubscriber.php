7f76db75a3af4c34adebe8695b004331883ada7e
Richard Haeser <richard@richardhaeser.com>
2025-04-10 20:23:47 +0200

    wip

--- src/Subscriber/StorefrontRenderSubscriber.php
+++ src/Subscriber/StorefrontRenderSubscriber.php
@@ -10,26 +10,32 @@ use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Shopware\Core\Framework\Struct\ArrayStruct;
+use Shopware\Core\Framework\Uuid\Uuid;
 use Shopware\Storefront\Event\StorefrontRenderEvent;
 use Shopware\Storefront\Framework\Twig\ErrorTemplateStruct;
 use Shopware\Storefront\Page\Page;
 use Shopware\Storefront\Page\Product\ProductPage;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class StorefrontRenderSubscriber implements EventSubscriberInterface
 {
     private EntityRepository $frontendRepository;
     private NavigationLoader $navigationLoader;
     private ProductDataService $productDataService;
+    private RequestStack $requestStack;
 
     public function __construct(
         EntityRepository $frontendRepository,
         NavigationLoader $navigationLoader,
-        ProductDataService $productDataService
+        ProductDataService $productDataService,
+        RequestStack $requestStack
     ) {
         $this->frontendRepository = $frontendRepository;
         $this->navigationLoader = $navigationLoader;
         $this->productDataService = $productDataService;
+        $this->requestStack = $requestStack;
     }
 
     public static function getSubscribedEvents()
@@ -61,8 +67,17 @@ class StorefrontRenderSubscriber implements EventSubscriberInterface
             $this->parseCategoryData($categoryData, $domainId, $treeItem);
         }
 
+        $session = $this->requestStack->getCurrentRequest()->getSession();
+        $profileKey = $session->get('tweakwise_profile_key');
+
+        if (!$profileKey) {
+            $profileKey = Uuid::randomHex();
+            $session->set('tweakwise_profile_key', $profileKey);
+        }
+
         $twConfiguration = [
             'domainId' => $domainId,
+            'profileKey' => $profileKey,
             'rootCategoryId' => $rootCategoryId,
             'instanceKey' => $result->getToken(),
             'integration' => $result->getIntegration(),
