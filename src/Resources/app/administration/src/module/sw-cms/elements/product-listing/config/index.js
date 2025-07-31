27dd8205f777fcc508eef1c6b913d6cc44e1c7f5
Richard Haeser <richard@richardhaeser.com>
2025-07-31 11:52:10 +0200

    [BUGFIX] Fix issue not able to select tweakwise listing

--- src/Resources/app/administration/src/module/sw-cms/elements/product-listing/config/index.js
+++ src/Resources/app/administration/src/module/sw-cms/elements/product-listing/config/index.js
@@ -1,5 +1,32 @@
 import template from './sw-cms-el-config-product-listing.html.twig';
 
 Shopware.Component.override('sw-cms-el-config-product-listing', {
-    template
+    template,
+
+    computed: {
+        boxLayoutOptions() {
+            return [
+                {
+                    id: 1,
+                    value: 'standard',
+                    label: this.$tc('sw-cms.elements.productBox.config.label.layoutTypeStandard'),
+                },
+                {
+                    id: 2,
+                    value: 'image',
+                    label: this.$tc('sw-cms.elements.productBox.config.label.layoutTypeImage'),
+                },
+                {
+                    id: 3,
+                    value: 'minimal',
+                    label: this.$tc('sw-cms.elements.productBox.config.label.layoutTypeMinimal'),
+                },
+                {
+                    id: 50,
+                    value: 'tweakwise',
+                    label: this.$tc('sw-cms.elements.productBox.config.label.layoutTypeTweakwise'),
+                },
+            ];
+        }
+    }
 });
