e94570722194da4e519b9b0d993e9137eda08bd3
Richard Haeser <richard@richardhaeser.com>
2025-12-09 15:10:36 +0100

    [TASK] Only show Tweakwise tab on product editing screen when backend sync is enabled for at least 1 instance

--- src/Resources/app/administration/src/module/sw-product/page/sw-product-detail/index.js
+++ src/Resources/app/administration/src/module/sw-product/page/sw-product-detail/index.js
@@ -1,5 +1,42 @@
 import template from "./sw-product-detail.html.twig";
 
+const { Criteria } = Shopware.Data;
+
 Shopware.Component.override('sw-product-detail', {
     template,
+    inject: ['repositoryFactory', 'feature', 'acl'],
+
+    data() {
+        return {
+            frontendCount: 0,
+            isLoadingFrontends: false,
+        }
+    },
+
+    computed: {
+        twFeedRepository() {
+            return this.repositoryFactory.create('s_plugin_rhae_tweakwise_frontend');
+        },
+        hasEnabledFrontends() {
+            return this.frontendCount > 0;
+        }
+    },
+    methods: {
+        async createdComponent() {
+            this.$super('createdComponent');
+
+            this.isLoadingFrontends = true;
+
+            try {
+                const criteria = new Criteria(1, 1);
+                criteria.addFilter(Criteria.equals('backendSyncEnabled', true));
+
+                const result = await this.twFeedRepository.search(criteria, Shopware.Context.api);
+
+                this.frontendCount = result.total;
+            } finally {
+                this.isLoadingFrontends = false;
+            }
+        },
+    }
 });
