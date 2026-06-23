54ab570346b102c2188fc4a73d3fa71b181b4790
Richard Haeser <richard@richardhaeser.com>
2026-06-23 12:55:13 +0200

    [FEATURE] Attribute Landing Pages

---
+++ src/Resources/app/administration/src/module/sw-cms/elements/tweakwise/attribute-landing-page/component/index.js
@@ -0,0 +1,6 @@
+import template from './sw-cms-el-tweakwise-attribute-landing-page.html.twig';
+import './sw-cms-el-tweakwise-attribute-landing-page.scss';
+
+Shopware.Component.register('sw-cms-el-tweakwise-attribute-landing-page', {
+    template
+});
